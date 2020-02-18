a13d5c0a4d6b0b0bfb5093c6bd08056674b04538
ml1150258 <mark.lehr@rcc.edu>
2020-02-18 14:14:39 -0800

    Signed-off-by: ml1150258 <mark.lehr@rcc.edu>

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,35 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on February 18, 2020, 11:33 AM
+ * Purpose: C++ Template
+ */
+
+//System Level Libraries
+#include <iostream>   //I/O Library
+using namespace std;  //Libraries compiled under std
+
+//User Level Libraries
+
+//Global Constants - Science/Math Related
+//Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Random Number Seed Set Here
+    
+    //Variable Declarations
+    
+    //Variable Initialization
+    
+    //Mapping Process Inputs to Outputs
+    
+    //Display Outputs
+
+    //Clean Up
+    
+    //Exit stage right!
+    return 0;
+}
---
+++ Class/HelloWorld/main.cpp
@@ -0,0 +1,41 @@
+/* 
+ * File:   main.cpp
+ * Author: Dr. Mark E. Lehr
+ * Created on February 18, 2020, 11:33 AM
+ * Purpose: Hello World
+ */
+
+//System Level Libraries
+#include <iostream>   //I/O Library
+using namespace std;  //Libraries compiled under std
+
+//User Level Libraries
+
+//Global Constants - Science/Math Related
+//Conversions, Higher Dimensions
+
+//Function Prototypes
+
+//Execution Begins Here!
+int main(int argc, char** argv) {
+    //Random Number Seed Set Here
+    
+    //Variable Declarations
+    
+    //Variable Initialization
+    
+    //Mapping Process Inputs to Outputs
+    
+    //Display Outputs
+    cout<<"Hello World"<<endl;//All in 1 Line
+    cout<<"Hello World";//All in 2 Lines
+    cout<<endl;
+    cout<<"Hello ";//All in 3 Lines
+    cout<<"World";
+    cout<<endl;
+
+    //Clean Up
+    
+    //Exit stage right!
+    return 0;
+}
